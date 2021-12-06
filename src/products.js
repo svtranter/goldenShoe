41503b4136e653ad3c06a320af96cba720094f4e
svtranter <82532670+svtranter@users.noreply.github.com>
2021-12-06 21:01:45 +0000

    Buttons now run addToCart

--- src/products.js
+++ src/products.js
@@ -18,61 +18,59 @@ function Products() {
         },
         {
             id: 2,
-            name: "Not Saddle Loafers",
+            name: "Saddle Loafers 2: Electric Boogaloo",
             imgsrc: featuredShoes,
             description: "The track sole makes the shoe look casual and versatile. It goes perfect with a suit or a pretty summer dress.",
             price: 49.99
         },
-        // {
-        //     id: 3,
-        //     name: "Totally Not Saddle Loafers",
-        //     imgsrc: featuredShoes,
-        //     description: "The track sole makes the shoe look casual and versatile. It goes perfect with a suit or a pretty summer dress.",
-        //     price: 49.99
-        // },
-        // {
-        //     id: 4,
-        //     name: "Saddle Loafers",
-        //     imgsrc: featuredShoes,
-        //     description: "The track sole makes the shoe look casual and versatile. It goes perfect with a suit or a pretty summer dress.",
-        //     price: 49.99
-        // },
-        // {
-        //     id: 5,
-        //     name: "Saddle Loafers",
-        //     imgsrc: featuredShoes,
-        //     description: "The track sole makes the shoe look casual and versatile. It goes perfect with a suit or a pretty summer dress.",
-        //     price: 49.99
-        // },
-        // {
-        //     id: 6,
-        //     name: "Saddle Loafers",
-        //     imgsrc: featuredShoes,
-        //     description: "The track sole makes the shoe look casual and versatile. It goes perfect with a suit or a pretty summer dress.",
-        //     price: 49.99
-        // }
+        {
+            id: 3,
+            name: "Return Of The Saddle Loafers",
+            imgsrc: featuredShoes,
+            description: "The track sole makes the shoe look casual and versatile. It goes perfect with a suit or a pretty summer dress.",
+            price: 49.99
+        },
+        {
+            id: 4,
+            name: "Saddle Loafers Reloaded",
+            imgsrc: featuredShoes,
+            description: "The track sole makes the shoe look casual and versatile. It goes perfect with a suit or a pretty summer dress.",
+            price: 49.99
+        },
+        {
+            id: 5,
+            name: "Saddle Loafers Endgame",
+            imgsrc: featuredShoes,
+            description: "The track sole makes the shoe look casual and versatile. It goes perfect with a suit or a pretty summer dress.",
+            price: 49.99
+        },
+        {
+            id: 6,
+            name: "Saddle Loafers And The Half-Blood Prince",
+            imgsrc: featuredShoes,
+            description: "The track sole makes the shoe look casual and versatile. It goes perfect with a suit or a pretty summer dress.",
+            price: 49.99
+        }
     ]
 
-    const cart = [{
-        id: 1,
-        name: "Saddle Loafers",
-        imgsrc: featuredShoes,
-        description: "The track sole makes the shoe look casual and versatile. It goes perfect with a suit or a pretty summer dress.",
-        price: 49.99
-    }]
+    const cart = [];
 
     const addToCart = (id) => {
         console.log(id);
         console.log(cart);
         const check_index = cart.findIndex(item => item.id === id);
         if (check_index !== -1) {
+            
             cart[check_index].quantity++;
+            
             console.log("Quantity updated:", cart);
         } else {
+            
             cart.push({
                 ...products.find(p => p.id === id),
                 quantity: 1
             })
+            
             console.log("The product has been added to cart:", cart);
         }
     }
@@ -89,7 +87,7 @@ function Products() {
             </Card.Body>
             <Card.Footer>
             <small>£{card.price}{' '}</small>
-            <Button variant="danger" className="float-end" onClick={addToCart(card.id)} >Add to basket</Button>
+            <Button variant="danger" className="float-end" onClick={() => addToCart(card.id)} >Add to basket</Button>
             </Card.Footer>
         </Card>     
         );
